c6e5bf760fe2e8f4526fe5a0a0798ca92839fe1b
Celso Santos <66226608+CelsoSantos30@users.noreply.github.com>
2020-08-09 13:36:40 -0300

    Add files via upload

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20" , 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Daniele Evangelista", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20" , 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Sergio Leal", 
+        avatar: "https://avatars0.githubusercontent.com/u/69284237?s=460&u=b5587496ee081b0a5dcb77fdabcec4ca9ae12a9d&v=4",
+        whatsapp: "89987654534",
+        bio: "Entusiasta das melhores tecnologias da informação avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Desenvolvedor",
+        cost: "100" , 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    }    
+]
